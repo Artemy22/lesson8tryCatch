a07bfcffb0cb3cfb51fb5feb61f4d97a63eeb579
artemy22 <artem.nii@gmail.com>
2019-10-08 19:45:03 +0300

    1 is done

--- src/main/java/lesson8.java
+++ src/main/java/lesson8.java
@@ -1,9 +1,19 @@
+/*
+1. Create method to check:
+if number < 0 print “Number < 0”,
+if number > 0 print “Number > 0”
+if number = 0 throw your own exception
+ */
+
 public class lesson8 {
-    public static void main(String[] args) {
-        try {
-            throw new Exception ();
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        int number = 0;
+        if (number < 0) {
+            System.out.println("Number < 0");
+        } else if (number  > 0) {
+            System.out.println("Number > 0");
+        } else if (number == 0) {
+            throw new Exception();
         }
     }
 }
