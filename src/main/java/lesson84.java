e8749e6a9b16d000aeeab07eda33430c4cf66a6d
artemy22 <artem.nii@gmail.com>
2019-10-11 10:33:34 +0300

    3 is fixed, right exception

--- src/main/java/lesson84.java
+++ src/main/java/lesson84.java
@@ -6,14 +6,14 @@ Print the cause of the exception to the console."
 public class lesson83 {
     public static void main(String[] args) {
         try {
-//            int a = args.length;
-//            System.out.println(a);
-//
-//            int b = 10 / a;
+            int a = args.length;
+            System.out.println(a);
+
+            int b = 10 / a;
             int[] c = {1};
-            System.out.println(c[1]);
+            System.out.println(c[0]);
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 }
