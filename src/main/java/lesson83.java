96d9cd3dc223b0e26782a7f76b6d92e5cd95df71
artemy22 <artem.nii@gmail.com>
2019-10-11 10:17:43 +0300

    3 is fixed

--- src/main/java/lesson83.java
+++ src/main/java/lesson83.java
@@ -4,21 +4,16 @@ Print the cause of the exception to the console."
  */
 
 public class lesson83 {
-    public static class Main {
+    public static void main(String[] args) {
+        try {
+            int a = args.length;
+            System.out.println(a);
 
-        public static void main(String[] args) {
-
-            try {
-                int a = args.length;
-                System.out.println(a);
-
-                int b = 10 / a;
-                int[] c = {1};
-                System.out.println(c[0]);
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            int b = 10 / a;
+            int[] c = {1};
+            System.out.println(c[0]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
